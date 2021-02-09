a3419762619ea16058dae37608bff1c7efcf542c
aznoH333 <62841387+aznoH333@users.noreply.github.com>
2021-02-09 16:02:48 +0100

    deleni 0 uz se nedeje plne funkcni

--- src/main/java/cz/educanet/tranformations/logic/models/Line.java
+++ src/main/java/cz/educanet/tranformations/logic/models/Line.java
@@ -6,17 +6,20 @@ public class Line {
     private double multiplier;
     private double addition;
     private boolean direction = false;
+    private boolean infinityWorkAround = false;
 
     public Line(Coordinate cord1, Coordinate cord2) {
 
 
         if ((cord2.getX() - cord1.getX()) == 0) {
-            multiplier = 0; //temporary fix
+            infinityWorkAround = true;
+            addition = cord1.getX();
         } else {
             multiplier = (double) (cord2.getY() - cord1.getY()) / (double) (cord2.getX() - cord1.getX());
+            addition = cord1.getY() - (multiplier * cord1.getX());
         }
 
-        addition = cord1.getY() - (multiplier * cord1.getX());
+
 
     }
 
@@ -29,10 +32,21 @@ public class Line {
     }
 
     public boolean isInside(Coordinate coordinate) {
-        if (direction) {
-            return coordinate.getY() >= multiplier * coordinate.getX() + addition;
-        } else {
-            return coordinate.getY() <= multiplier * coordinate.getX() + addition;
+        if(!infinityWorkAround){
+            //normal operation
+            if (direction) {
+                return coordinate.getY() >= multiplier * coordinate.getX() + addition;
+            } else {
+                return coordinate.getY() <= multiplier * coordinate.getX() + addition;
+            }
+        }else {
+            //working with infinity
+            if (direction) {
+                return coordinate.getX() >= addition;
+            } else {
+                return coordinate.getX() <= addition;
+            }
+
         }
 
     }
